6c85bdf295a074f10702167a5a00dfe1360dc6ae
Hai Phan <htp200006@gmail.com>
2020-10-11 20:24:13 -0500

    create demo page

--- angular-web-app/src/app/app-routing.module.ts
+++ angular-web-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DemoPageComponent} from "./demo-page/demo-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'demo-page',
+    component: DemoPageComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
